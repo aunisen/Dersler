0e798c8c25afc145c272300eaff41ba289abef87
aunisen <aunisen@hotmail.com>
2024-04-01 00:44:15 +0300

    proje eklendi

---
+++ Dersler.KutuphaneWpf/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dersler.KutuphaneWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(Mouse.LeftButton==MouseButtonState.Pressed)
+            {
+                this.DragMove();    
+            }
+        }
+
+        private void btn_kapat_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();   
+        }
+
+        private void btn_kucult_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState == WindowState.Normal) { this.WindowState = WindowState.Minimized; }
+            else if(this.WindowState==WindowState.Normal)this.WindowState= WindowState.Normal;
+
+        }
+
+        private void Window_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal) { this.WindowState = WindowState.Maximized; }
+            else if (this.WindowState == WindowState.Maximized) this.WindowState = WindowState.Normal;
+        }
+    }
+}
